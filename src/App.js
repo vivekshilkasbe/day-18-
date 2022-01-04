0213eba21e6f45a15054c21df9b236eed8f23ddf
vivekshilkasbe <vikky0358@gmail.com>
2022-01-05 01:43:51 +0530

    componenet

--- src/App.js
+++ src/App.js
@@ -1,54 +1,33 @@
 export default function App() {
-  let list = [1, 1, 1, 1, 1];
-
   return (
-    <div className="container-fluid">
-      <Header />
-
-      <div className="row">
-        {list.map(() => (
-          <div className="col-sm-12 col-md-4 mb-2">
-            <Mycard />
-          </div>
-        ))}
-      </div>
-    </div>
+    <>
+      <MyComponent />
+      <MyComponent />
+      <MyComponent />
+    </>
   );
 }
-function Header() {
-  return (
-    <div className="row bg-dark text-light p-3 ">
-      <div className="col h3">Sound Of Soul</div>
-    </div>
-  );
-}
-function Mycard() {
-  let cardTitle = "card Title";
-  return (
-    <div className="card p-2">
-      <div className="card-header">{cardTitle}</div>
-      <div classname="card-body">
-        {/* <img src="https://picsum.photos/450/300"></img> */}
-        <div>
-          NPM is short for node package manager, an online directory that
-          contains the various already registered open-source packages. NPM
-          modules consume the various functions as a third-party package when
-          installed into an app using the NPM command npm install .
-        </div>
 
-        <div>
-          <input
-            className="btn btn-sm btn-outline-primary w-50 mt-2"
-            type="button"
-            value="Buy Now"
-          />
-          <input
-            className="btn btn-sm btn-outline-primary w-50 mt-2"
-            type="button"
-            value="Add To Cart"
-          />
-        </div>
-      </div>
+function MyComponent() {
+  let username = "Vivekshil";
+  let email = "vikky0358@gmail.com";
+  let id = 100;
+  let list = ["delhi", "Culcutta"];
+  //carefull working with object
+  let obj = { id: 1, name: "Vivek" };
+  return (
+    <div>
+      <h1>Welcome {"Delhi".toUpperCase()}</h1>
+      <h1>{username} </h1>
+      <h1>{list} </h1>
+      <h1>
+        {obj.id}
+        {obj.name}
+        {` i am also js ${obj.id} and  ${obj.name}`}{" "}
+      </h1>
+      <p>
+        loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
+      </p>
     </div>
   );
 }
