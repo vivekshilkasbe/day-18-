2a1a3ad922e3d00724aae74f384b167e11623b86
vivekshilkasbe <vikky0358@gmail.com>
2022-01-05 01:13:12 +0530

    web app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-/**
- *
- * @returns jsx => xml
- */
-import HelloWorld from "./components/HelloWorld.js";
-
-import HelloUniverse from "./components/HelloUniverse.js";
-import { HelloWorld1, HelloWorld2 } from "./components/HelloWorld.js";
-
 export default function App() {
-  return <div>Hello World</div>;
+  let list = [1, 1, 1, 1, 1];
+
+  return (
+    <div className="container-fluid">
+      <Header />
+
+      <div className="row">
+        {list.map(() => (
+          <div className="col-sm-12 col-md-4 mb-2">
+            <Mycard />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+function Header() {
+  return (
+    <div className="row bg-dark text-light p-3 ">
+      <div className="col h3">Sound Of Soul</div>
+    </div>
+  );
+}
+function Mycard() {
+  let cardTitle = "card Title";
+  return (
+    <div className="card p-2">
+      <div className="card-header">{cardTitle}</div>
+      <div classname="card-body">
+        {/* <img src="https://picsum.photos/450/300"></img> */}
+        <div>
+          NPM is short for node package manager, an online directory that
+          contains the various already registered open-source packages. NPM
+          modules consume the various functions as a third-party package when
+          installed into an app using the NPM command npm install .
+        </div>
+
+        <div>
+          <input
+            className="btn btn-sm btn-outline-primary w-50 mt-2"
+            type="button"
+            value="Buy Now"
+          />
+          <input
+            className="btn btn-sm btn-outline-primary w-50 mt-2"
+            type="button"
+            value="Add To Cart"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
