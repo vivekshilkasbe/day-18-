5d463099617069c9199622e81f9c3c87b29e5187
vivekshilkasbe <vikky0358@gmail.com>
2022-01-06 02:20:41 +0530

    ful form validation

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ export default function App() {
   return (
     <>
       <MyComponent />
+      {/* <MyComponent /> */}
     </>
   );
 }
 
 function MyComponent() {
+  const [validationError, setvalidationError] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [mobile, setMobile] = useState("");
+
+  const [userList, setUserList] = useState([]);
 
   const handleUsername = (e) => {
     setUsername(e.target.value);
@@ -27,8 +31,28 @@ function MyComponent() {
     setMobile(e.target.value);
   };
   const registerUser = () => {
-    alert(`${username} ${password} ${email} ${mobile} `);
+    // alert(`${username} ${password} ${email} ${mobile} `);
+    if (username == "" || password == "" || email == "" || mobile == "") {
+      setvalidationError(true);
+      return;
+    }
+    const user = {
+      username: username,
+      password: password,
+      email: email,
+      mobile: mobile,
+    };
+    const newList = [user, ...userList];
+    setUserList(newList);
+
+    // clear in the end
+    setUsername("");
+    setPassword("");
+    setEmail("");
+    setMobile("");
+    setvalidationError(false);
   };
+
   return (
     <div>
       <h1>Working with Registration </h1>
@@ -38,6 +62,9 @@ function MyComponent() {
           placeholder="enter Username"
           value={username}
           onChange={handleUsername}
+          className={
+            username == "" && validationError ? "border border-danger" : ""
+          }
         />
       </div>
       <div>
@@ -46,6 +73,9 @@ function MyComponent() {
           placeholder="enter password"
           value={password}
           onChange={handlePassword}
+          className={
+            password == "" && validationError ? "border border-danger" : ""
+          }
         />
       </div>
       <div>
@@ -54,6 +84,9 @@ function MyComponent() {
           placeholder="enter Email"
           value={email}
           onChange={handleEmail}
+          className={
+            email == "" && validationError ? "border border-danger" : ""
+          }
         />
       </div>
       <div>
@@ -62,11 +95,20 @@ function MyComponent() {
           placeholder="enter mobile"
           value={mobile}
           onChange={handleMobile}
+          className={
+            mobile == "" && validationError ? "border border-danger" : ""
+          }
         />
       </div>
       <div>
         <input type="button" value="Registration" onClick={registerUser} />
       </div>
+      <hr />
+      {userList.map((item, index) => (
+        <div key={index}>
+          {item.username},{item.password},{item.email},{item.mobile}
+        </div>
+      ))}
     </div>
   );
 }
