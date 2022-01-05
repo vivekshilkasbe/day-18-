e3338e0e028810499bf71c29165b5c703af7441b
vivekshilkasbe <vikky0358@gmail.com>
2022-01-05 15:06:14 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
+import { useState } from "react";
+
 export default function App() {
   return (
     <>
-      <MyComponent />
-      <MyComponent />
       <MyComponent />
     </>
   );
 }
 
 function MyComponent() {
-  let username = "Vivekshil";
-  let email = "vikky0358@gmail.com";
-  let id = 100;
-  let list = ["delhi", "Culcutta"];
-  //carefull working with object
-  let obj = { id: 1, name: "Vivek" };
+  const id = 100;
+  // const cardCss = "bg-info text-light fs-3 p-4 mb-1";
+  const list = [1, 1, 1];
+  const [cardCss, setCardcss] = useState("bg-primary text-light fs-3 p-4 mb-1");
+  const primaryTheme = () => setCardcss("bg-info text-light fs-3 p-4 mb-1");
+  const dangerTheme = () => setCardcss("bg-danger text-light fs-3 p-4 mb-1");
+  const darkTheme = () => setCardcss("bg-dark text-light fs-3 p-4 mb-1");
   return (
     <div>
-      <h1>Welcome {"Delhi".toUpperCase()}</h1>
-      <h1>{username} </h1>
-      <h1>{list} </h1>
-      <h1>
-        {obj.id}
-        {obj.name}
-        {` i am also js ${obj.id} and  ${obj.name}`}{" "}
-      </h1>
-      <p>
-        loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
-      </p>
+      <input
+        type="button"
+        value="PrimaryTheme"
+        className="btn btn-primary btn-sm"
+        onClick={primaryTheme}
+      />
+      <input
+        type="button"
+        value="DangerTheme"
+        className="btn btn-danger btn-sm"
+        onClick={dangerTheme}
+      />
+      <input
+        type="button"
+        value="DarkTheme"
+        className="btn btn-dark btn-sm"
+        onClick={darkTheme}
+      />
+      {list.map(() => (
+        <div className={cardCss}>
+          WPT Session- Shared screen with speaker view
+        </div>
+      ))}
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <h1>Interpolation {id} </h1>
+  //     <h1>Interpolation {id + 100} </h1>
+  //     <h1 id="1" className="bg-primary">
+  //       Interpolation {id + 100}
+  //     </h1>
+  //     <h1 id={id} className={background}>
+  //       Interpolation {id + 100}{" "}
+  //     </h1>
+  //   </div>
+  // );
 }
