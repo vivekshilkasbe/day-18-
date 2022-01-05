5a60d88465a0be8254013f11da243c4694b2f594
vivekshilkasbe <vikky0358@gmail.com>
2022-01-06 01:58:55 +0530

    register

--- src/App.js
+++ src/App.js
@@ -9,36 +9,64 @@ export default function App() {
 }
 
 function MyComponent() {
-  //1. 2= value={message}
-  const [message, setMessage] = useState("");
-  const [messageList, setMessageList] = useState([]);
-  //3.
-  const processMessage = (e) => {
-    const newMessage = e.target.value;
-    setMessage(newMessage);
-  };
-  //4.
-  const addTweet = () => {
-    console.log(message);
-    const newMessageList = [message, ...messageList];
-    setMessageList(newMessageList);
-    setMessage("");
-  };
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
 
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMobile = (e) => {
+    setMobile(e.target.value);
+  };
+  const registerUser = () => {
+    alert(`${username} ${password} ${email} ${mobile} `);
+  };
   return (
     <div>
-      <h1>Input Operation</h1>
-      <input
-        type="text"
-        name=""
-        value={message}
-        placeholder="whats app"
-        onChange={processMessage}
-      />
-      <input type="button" value="tweet" onClick={addTweet} />
-      {messageList.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))}
+      <h1>Working with Registration </h1>
+      <div>
+        <input
+          type="text"
+          placeholder="enter Username"
+          value={username}
+          onChange={handleUsername}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="enter password"
+          value={password}
+          onChange={handlePassword}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="enter Email"
+          value={email}
+          onChange={handleEmail}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="enter mobile"
+          value={mobile}
+          onChange={handleMobile}
+        />
+      </div>
+      <div>
+        <input type="button" value="Registration" onClick={registerUser} />
+      </div>
     </div>
   );
 }
